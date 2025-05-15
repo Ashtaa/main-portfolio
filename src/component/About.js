419967405d3d529fcaeb15e429a67b3e825d96c1
Ashtaa <ashenafisahele@gmail.com>
2025-05-15 13:19:33 +0300

    Update

--- src/component/About.js
+++ src/component/About.js
@@ -1,7 +1,50 @@
 import { FaGithub, FaLinkedin,FaFileCode,FaNodeJs,FaServer,FaDatabase,FaGitAlt,FaReact } from "react-icons/fa"
 import "./About.css"
+import react from './../img/advanced-react.png'
+import ui_ux from './../img/coursera-ui-ux.png'
+import hackathon from './../img/hospitality-hackathon.png'
+
 
 const About = ({theme}) => {
+  
+ const downloadCertificate = (pdfPath) => {
+  const link = document.createElement("a");
+  link.href = pdfPath;
+  link.download = pdfPath.split("/").pop(); // e.g., cert-1.pdf
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
+   const cirtificates = [
+      {
+        id: 1,
+        title: "Advanced-ract",
+        description:
+          "a cirtificate which i recive through coursera.",
+        image: react,
+         pdf:'/Advanced react .pdf'
+      },
+      {
+        id: 2,
+        title: "UI/UX",
+        description:
+          "the course which increase and developed my designing skills.",
+        image: ui_ux,
+        pdf:'/Coursera ui ux.pdf'
+        
+      },
+      {
+        id: 3,
+        title: "hackathon cirtificate",
+        description:
+          "A useful experiance which i learn multipe skill and which helped me to test my communication and team work skill",
+        image: hackathon,
+         pdf:'/Hospitality Hackathon 2025 Certeficate-37.pdf'
+       
+      }
+     
+    ];
   return (
     <div className={`about-container ${theme}`}>
       <div className={`about-content  ${theme} `}>
@@ -35,8 +78,22 @@ const About = ({theme}) => {
           <a href="https://linkedin.com/in/ashenafisahele" aria-label="LinkedIn" className="social-link">
             <FaLinkedin size={32} />
           </a>
+          
         </div>
       </div>
+
+    <div className="cirtificate-container">
+  {cirtificates.map((certificate, index) => (
+    <div key={index} className="certificate-card">
+      <h2>{certificate.title}</h2>
+      <img src={certificate.image} alt={certificate.title} className="cirtificate-img" />
+      <p>{certificate.description}</p>
+      
+      <button onClick={() => downloadCertificate(certificate.pdf)} className="download-btn">download PDF</button>
+    </div>
+  ))}
+</div>
+
     </div>
   )
 }
